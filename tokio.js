f242c82d13bf6b87a2f5dda217ba77310818ee19
JusTWizZ <87236534+JusTWizZ@users.noreply.github.com>
2022-03-26 19:31:05 +0100

    Add files via upload

---
+++ tokio.js
@@ -0,0 +1,24 @@
+function Sonido(){
+    var vid = document.getElementById("video2");
+    var vid1 = document.getElementById("switch-label1")
+    vid.setAttribute("src","https://www.youtube.com/embed/qgfd-uWTVwg?autoplay=1&mute=0&controls=0&start=60&playsinline=1&rel=0&showinfo=0&modestbranding:1&disablekb=0")
+    vid1.setAttribute("onclick","Silenciar()");
+}
+function Silenciar(){
+    var vid = document.getElementById("video2");
+    var vid1 = document.getElementById("switch-label1")
+    vid.setAttribute("src","https://www.youtube.com/embed/qgfd-uWTVwg?autoplay=1&mute=1&controls=0&start=60&playsinline=1&rel=0&showinfo=0&modestbranding:1&disablekb=0")
+    vid1.setAttribute("onclick","Sonido()");
+}
+function SonidoMusica(){
+    var vid = document.getElementById("musica");
+    var vid1 = document.getElementById("switch-label")
+    vid.muted = false;
+    vid1.setAttribute("onclick","SilenciarMusica()");
+}
+function SilenciarMusica(){
+    var vid = document.getElementById("musica");
+    var vid1 = document.getElementById("switch-label")
+    vid.muted = true;
+    vid1.setAttribute("onclick","SonidoMusica()");
+}
